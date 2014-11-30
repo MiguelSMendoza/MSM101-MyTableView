e7d81e60594e378c3cbe9474b9f8ece1f4087d45
Miguel S. Mendoza <miguelsmendoza@server.villodre.private>
2014-11-30 18:30:12 +0100

    Initial Commit

---
+++ MyTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTableView
+//
+//  Created by Miguel S. Mendoza on 30/11/14.
+//  Copyright (c) 2014 NetRunners. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTableView
+//
+//  Created by Miguel S. Mendoza on 30/11/14.
+//  Copyright (c) 2014 NetRunners. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
